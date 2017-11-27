6ab4e79aee1578aea45dc97d9391b11162297e75
Stas <sromanenko91@gmail.com>
2017-11-27 18:04:59 +0300

    создал класс Router прописал route к webhook

--- web/index.php
+++ web/index.php
@@ -1,2 +1,14 @@
 <?php
-require_once '../spl_autoload_reg.php';
+//require_once '../spl_autoload_reg.php';
+
+//Front controller
+
+// 1. Общие настройки
+
+// 2. Подключение файлов системы
+require_once __DIR__ . '/../components/Router.php';
+// 3. Соединение с БД
+
+// 4. Вызов Router
+$router = new Router();
+$router->run();
