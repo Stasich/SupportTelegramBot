6b8bf9f2933b891babf8180f779346c07aa02856
Stas <sromanenko91@gmail.com>
2017-12-04 05:06:17 +0300

    Получаю и добавляю в БД данные от webhook

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -2,10 +2,14 @@
 
 namespace Controllers;
 
+use Models\Stasich\SupBot;
+
 class WebhookController
 {
     public function indexAction()
     {
-
+        $json = file_get_contents("php://input");
+        $supBot = new SupBot();
+        $supBot->addWebhookDataToDB($json);
     }
 }
